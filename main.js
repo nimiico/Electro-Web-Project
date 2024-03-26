1614d5bb45219059b8e1c4a1fa30827d4ebb6ba9
Nima Eslami <nimaeslami206@gmail.com>
2024-03-26 06:39:45 +0330

    Complete Header section and Topseller section.

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-const tabHeadingAll = document.querySelectorAll(
-  ".section-bestseller .tab-heading"
+const tabHeadingAll = document.querySelectorAll(".tab-heading");
+
+const tabContentAll = document.querySelectorAll(".tab-content");
+
+const topsellerTabHeadingAll = document.querySelectorAll(
+  ".topseller-tab-heading"
 );
 
-const tabContentAll = document.querySelectorAll(
-  ".section-bestseller .tab-content"
+const topsellerTabContentAll = document.querySelectorAll(
+  ".topseller-tab-content"
 );
 
 const removeAllActive = () => {
@@ -19,6 +23,23 @@ tabHeadingAll.forEach((h, i) => {
   h.addEventListener("click", () => {
     removeAllActive();
     tabContentAll[i].classList.add("active");
+    h.classList.add("active");
+  });
+});
+
+const topsellerRemoveAllActive = () => {
+  topsellerTabContentAll.forEach((c) => {
+    c.classList.remove("active");
+  });
+
+  topsellerTabHeadingAll.forEach((h) => {
+    h.classList.remove("active");
+  });
+};
+topsellerTabHeadingAll.forEach((h, i) => {
+  h.addEventListener("click", () => {
+    topsellerRemoveAllActive();
+    topsellerTabContentAll[i].classList.add("active");
     h.classList.add("active");
   });
 });

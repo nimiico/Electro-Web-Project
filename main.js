c82f250ced06a7135089b80fb3c8dde72e7ab7e3
Nima Eslami <nimaeslami206@gmail.com>
2024-03-28 20:11:28 +0330

    OTP and Password Changed Successfully have been added.

--- main.js
+++ main.js
@@ -58,11 +58,20 @@ btnNavEl.addEventListener("click", function () {
 /////////////////////////////////////////
 // LOGIN & SIGNUP //
 ////////////////////////////////////////
-
-const btnOPenLoginEl = document.querySelector(".login-btn");
-const btnCloseLoginEl = document.querySelector(".close-box");
-
 const btnLoginEl = document.querySelector(".log-login-btn");
+const btnOPenLoginEl = document.querySelector(".login-btn");
+const btnOPenSignupEl = document.querySelector(".log-register-btn");
+const btnOPenOTPEl = document.querySelector(".reg-register-btn");
+const btnSendOTPEl = document.querySelector(".send-otp");
+const btnOpenSuccessEl = document.querySelector(".forget-btn");
+const btnSuccessBackLoginEl = document.querySelector(
+  ".success-Change-pass-btn"
+);
+
+const btnCloseLoginEl = document.querySelector(".close-box");
+const btnClosesignupEl = document.querySelector(".s-close-box");
+const btnCloseOTPEl = document.querySelector(".otp-close-box");
+
 const herorEl = document.querySelector(".section-hero");
 
 btnOPenLoginEl.addEventListener("click", function () {
@@ -77,8 +86,16 @@ btnLoginEl.addEventListener("click", function () {
   herorEl.classList.remove("login-open");
 });
 
-const btnOPenSignupEl = document.querySelector(".log-register-btn");
-const btnClosesignupEl = document.querySelector(".s-close-box");
+btnOpenSuccessEl.addEventListener("click", function () {
+  herorEl.classList.remove("login-open");
+  herorEl.classList.add("success-open");
+});
+
+btnSuccessBackLoginEl.addEventListener("click", function () {
+  herorEl.classList.remove("success-open");
+  herorEl.classList.add("login-open");
+});
+
 btnOPenSignupEl.addEventListener("click", function () {
   herorEl.classList.remove("login-open");
   herorEl.classList.add("signup-open");
@@ -87,3 +104,92 @@ btnOPenSignupEl.addEventListener("click", function () {
 btnClosesignupEl.addEventListener("click", function () {
   herorEl.classList.remove("signup-open");
 });
+
+btnOPenOTPEl.addEventListener("click", function () {
+  herorEl.classList.remove("signup-open");
+  herorEl.classList.add("otp-open");
+});
+
+btnCloseOTPEl.addEventListener("click", function () {
+  herorEl.classList.remove("otp-open");
+});
+
+btnSendOTPEl.addEventListener("click", function () {
+  herorEl.classList.remove("otp-open");
+});
+
+/////////////////////////////////////////
+// OTP //
+////////////////////////////////////////
+
+const input = document.querySelectorAll(".input");
+const inputField = document.querySelector(".inputfield");
+let inputCount = 0,
+  finalInput = "";
+
+//Update input
+const updateInputConfig = (element, disabledStatus) => {
+  element.disabled = disabledStatus;
+  if (!disabledStatus) {
+    element.focus();
+  } else {
+    element.blur();
+  }
+};
+
+input.forEach((element) => {
+  element.addEventListener("keyup", (e) => {
+    e.target.value = e.target.value.replace(/[^0-9]/g, "");
+    let { value } = e.target;
+
+    if (value.length == 1) {
+      updateInputConfig(e.target, true);
+      if (inputCount <= 4 && e.key != "Backspace") {
+        finalInput += value;
+        if (inputCount < 4) {
+          updateInputConfig(e.target.nextElementSibling, false);
+        }
+      }
+      inputCount += 1;
+    } else if (value.length == 0 && e.key == "Backspace") {
+      finalInput = finalInput.substring(0, finalInput.length - 1);
+      if (inputCount == 0) {
+        updateInputConfig(e.target, false);
+        return false;
+      }
+      updateInputConfig(e.target, true);
+      e.target.previousElementSibling.value = "";
+      updateInputConfig(e.target.previousElementSibling, false);
+      inputCount -= 1;
+    } else if (value.length > 1) {
+      e.target.value = value.split("")[0];
+    }
+  });
+});
+
+window.addEventListener("keyup", (e) => {
+  if (inputCount > 4) {
+    if (e.key == "Backspace") {
+      finalInput = finalInput.substring(0, finalInput.length - 1);
+      updateInputConfig(inputField.lastElementChild, false);
+      inputField.lastElementChild.value = "";
+      inputCount -= 1;
+    }
+  }
+});
+
+const validateOTP = () => {
+  alert("Success");
+};
+
+//Start
+const startInput = () => {
+  inputCount = 0;
+  finalInput = "";
+  input.forEach((element) => {
+    element.value = "";
+  });
+  updateInputConfig(inputField.firstElementChild, false);
+};
+
+window.onload = startInput();

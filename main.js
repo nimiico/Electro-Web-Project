fb1fafd70eb767eaa827e0a51db0cefdf28c86b7
Nima Eslami <nimaeslami206@gmail.com>
2024-03-28 08:13:14 +0330

    Login and Rgister have been added.

--- main.js
+++ main.js
@@ -43,3 +43,47 @@ topsellerTabHeadingAll.forEach((h, i) => {
     h.classList.add("active");
   });
 });
+
+/////////////////////////////////////////
+// MOBILE NAVIGATION MENU //
+////////////////////////////////////////
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+/////////////////////////////////////////
+// LOGIN & SIGNUP //
+////////////////////////////////////////
+
+const btnOPenLoginEl = document.querySelector(".login-btn");
+const btnCloseLoginEl = document.querySelector(".close-box");
+
+const btnLoginEl = document.querySelector(".log-login-btn");
+const herorEl = document.querySelector(".section-hero");
+
+btnOPenLoginEl.addEventListener("click", function () {
+  herorEl.classList.add("login-open");
+});
+
+btnCloseLoginEl.addEventListener("click", function () {
+  herorEl.classList.remove("login-open");
+});
+
+btnLoginEl.addEventListener("click", function () {
+  herorEl.classList.remove("login-open");
+});
+
+const btnOPenSignupEl = document.querySelector(".log-register-btn");
+const btnClosesignupEl = document.querySelector(".s-close-box");
+btnOPenSignupEl.addEventListener("click", function () {
+  herorEl.classList.remove("login-open");
+  herorEl.classList.add("signup-open");
+});
+
+btnClosesignupEl.addEventListener("click", function () {
+  herorEl.classList.remove("signup-open");
+});
